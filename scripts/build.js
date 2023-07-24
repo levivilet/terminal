ad88c676c1c9841b9123dfb7a4a361d443d0b35b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-07-24 22:08:03 +0200

    add github pages

---
+++ scripts/build.js
@@ -0,0 +1,12 @@
+import { cp, mkdir, rm } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const root = join(__dirname, "..");
+
+const dist = join(root, "dist");
+await rm(dist, { recursive: true, force: true });
+await mkdir(dist, { recursive: true });
+await cp(join(root, "src"), join(dist, "src"), { recursive: true });
+await cp(join(root, "index.html"), join(dist, "index.html"));
