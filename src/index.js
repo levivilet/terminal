6248312b4cd76bf5f0679e5424d2d8374689d241
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-07-24 22:18:02 +0200

    send offscreencanvas to worker

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
-const workerUrl = new URL("./worker.js", import.meta.url).toString();
-const worker = new Worker(workerUrl, {
-  name: "Terminal Worker",
-  type: "module",
-});
+const getWorkerUrl = () => {
+  const workerUrl = new URL("./worker.js", import.meta.url).toString();
+  return workerUrl;
+};
+
+const createRpc = (worker) => {
+  return {
+    worker,
+    invoke(method, params, transfer) {
+      const message = {
+        jsonrpc: "2.0",
+        method,
+        params,
+      };
+      this.worker.postMessage(message, transfer);
+    },
+  };
+};
+
+const main = async () => {
+  const workerUrl = getWorkerUrl();
+  const worker = new Worker(workerUrl, {
+    name: "Terminal Worker",
+    type: "module",
+  });
+  const canvas = document.querySelector("canvas");
+  const offscreenCanvas = canvas?.transferControlToOffscreen();
+  const rpc = createRpc(worker);
+  await rpc.invoke("addCanvas", [offscreenCanvas], [offscreenCanvas]);
+};
+
+main();
--- src/worker.js
+++ src/worker.js
@@ -1,6 +1,30 @@
-console.log("hello from worker");
+const state = {
+  canvas: undefined,
+};
 
-const handleMessage = () => {};
+const addCanvas = (canvas) => {
+  console.log({ canvas });
+  state.canvas = canvas;
+};
+
+const getFn = (method) => {
+  switch (method) {
+    case "addCanvas":
+      return addCanvas;
+    default:
+      throw new Error(`unexpected command`);
+  }
+};
+
+const executeCommand = (method, ...params) => {
+  const fn = getFn(method);
+  return fn(...params);
+};
+
+const handleMessage = (event) => {
+  const message = event.data;
+  executeCommand(message.method, ...message.params);
+};
 
 const main = () => {
   onmessage = handleMessage;
