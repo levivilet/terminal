d5c3b27064a8ab0f3aa2d4642c08094a6db2517f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-07-26 00:44:36 +0200

    remove unused code (#26)

--- packages/terminal-library/src/parts/TemporaryCtx/TemporaryCtx.js
+++
@@ -1,8 +0,0 @@
-export const create = (width, height) => {
-  const tmpCanvas = new OffscreenCanvas(width, height)
-  const tmpCtx = tmpCanvas.getContext('2d')
-  if (!tmpCtx) {
-    throw new Error(`Failed to create canvas`)
-  }
-  return { tmpCtx, tmpCanvas }
-}
