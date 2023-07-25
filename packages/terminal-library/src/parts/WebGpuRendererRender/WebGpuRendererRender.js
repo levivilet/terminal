9319c863fb79ce2c298a68700e3c2afb143173b3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-07-25 16:00:17 +0200

    refactor: split gpu renderer into separate files

---
+++ packages/terminal-library/src/parts/WebGpuRendererRender/WebGpuRendererRender.js
@@ -0,0 +1,38 @@
+export const render = (renderContext) => {
+  const {
+    device,
+    pipeline,
+    vertexBuffer,
+    vertices,
+    context,
+    bindGroup,
+    textureAtlas,
+    texture,
+  } = renderContext;
+  if (textureAtlas.modified) {
+    device.queue.copyExternalImageToTexture(
+      { source: textureAtlas.canvas },
+      { texture },
+      [textureAtlas.width, textureAtlas.height]
+    );
+    textureAtlas.modified = false;
+  }
+  const encoder = device.createCommandEncoder();
+  const pass = encoder.beginRenderPass({
+    colorAttachments: [
+      {
+        view: context.getCurrentTexture().createView(),
+        loadOp: "clear",
+        clearValue: { r: 1, g: 0.5, b: 0.4, a: 1.0 },
+        storeOp: "store",
+      },
+    ],
+  });
+  // Draw the square.
+  pass.setPipeline(pipeline);
+  pass.setBindGroup(0, bindGroup);
+  pass.setVertexBuffer(0, vertexBuffer);
+  pass.draw(vertices.length / 4);
+  pass.end();
+  device.queue.submit([encoder.finish()]);
+};
