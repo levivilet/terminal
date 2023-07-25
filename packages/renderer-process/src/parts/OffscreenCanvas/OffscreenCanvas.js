9eef8cc30f0de599884c6b7cae0b379be2d0f3d5
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-07-25 23:46:42 +0200

    feature: render tmp canvas on screen for debugging (#20)

--- packages/renderer-process/src/parts/OffscreenCanvas/OffscreenCanvas.js
+++ packages/renderer-process/src/parts/OffscreenCanvas/OffscreenCanvas.js
@@ -4,10 +4,17 @@ const offscreenCanvas = canvas.transferControlToOffscreen()
 const atlasCanvas = document.querySelector('#AtlasCanvas')
 const offscreenAtlasCanvas = atlasCanvas.transferControlToOffscreen()
 
+const tmpCanvas = document.querySelector('#TmpCanvas')
+const offscreenTmpCanvas = tmpCanvas.transferControlToOffscreen()
+
 export const create = () => {
   return offscreenCanvas
 }
 
 export const createAtlasCanvas = () => {
   return offscreenAtlasCanvas
+}
+
+export const createTmpCanvas = () => {
+  return offscreenTmpCanvas
 }
